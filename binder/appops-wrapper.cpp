ee2a82a5597a7ec234b93b5d98137b9e0f622102
Diogo Ferreira <defer@cyngn.com>
2016-09-07 10:58:12 +0100

    su: Use the AppOpsManager operation enum instead of hardcoding
    This should make it more maintainable in the future.
    
    Change-Id: Ica233d48c563dc5fc915d3a52a806228224e5503

--- binder/appops-wrapper.cpp
+++ binder/appops-wrapper.cpp
@@ -5,15 +5,13 @@
 
 using namespace android;
 
-#define APPOPS_OP_SU 69 // This must match the number in AppOpsManager
-
 extern "C" {
 
 int appops_start_op_su(int uid, const char *pkgName) {
     ALOGD("Checking whether app [uid:%d, pkgName: %s] is allowed to be root", uid, pkgName);
     AppOpsManager *ops = new AppOpsManager();
 
-    int mode = ops->startOp(APPOPS_OP_SU, uid, String16(pkgName));
+    int mode = ops->startOp(AppOpsManager::OP_SU, uid, String16(pkgName));
 
     switch (mode) {
         case AppOpsManager::MODE_ALLOWED:
@@ -30,7 +28,7 @@ int appops_start_op_su(int uid, const char *pkgName) {
 void appops_finish_op_su(int uid, const char *pkgName) {
     ALOGD("Finishing su operation for app [uid:%d, pkgName: %s]", uid, pkgName);
     AppOpsManager *ops = new AppOpsManager();
-    ops->finishOp(APPOPS_OP_SU, uid, String16(pkgName));
+    ops->finishOp(AppOpsManager::OP_SU, uid, String16(pkgName));
     free(ops);
 }
 
